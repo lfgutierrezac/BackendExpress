57d8b98783443d7b0d7484c5c53e002c780c9bb7
lufgutierrezac <fernandagac@gmail.com>
2021-10-27 14:11:27 -0500

    auth.service modification

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -9,7 +9,7 @@ const authService = {
             expiresIn: 60 * 60 * 24 * 365
         })
     },
-    login: async (data)=>{
+    login: async function(data){
         try {
             const {email,password} = data
             let userExists = await User.findOne({email:email}, 'name email password').exec()
@@ -26,7 +26,7 @@ const authService = {
             return error
         }
     },
-    register: async (userData)=>{
+    register: async function(userData){
         try {
             let hash = await bcrypt.hash(userData.password, 10).then(res => res)
             userData.password = hash
